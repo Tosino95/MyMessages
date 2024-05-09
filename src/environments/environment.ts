bc447ee3b10aece03d58817cf19882f04d6d59a4
Tosino95 <tosinonilogbo@gmail.com>
2024-05-09 12:29:18 -0500

    prepared for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl:
+    'http://mymessages-env-1.eba-wc5keh23.us-east-1.elasticbeanstalk.com/api',
 };
